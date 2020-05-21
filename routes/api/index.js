f0fae7365aa8c8918940f836c1cf745204660c86
Alvin Galit <aroblesgalit@gmail.com>
2020-05-21 14:10:31 -0500

    Added studentRoutes to api index file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
+const studentRoutes = require("./students");
 
 // Book routes
 router.use("/books", bookRoutes);
+router.use("/students", studentRoutes);
 
 module.exports = router;
