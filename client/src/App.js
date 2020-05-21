03f07785ae343a8884bec6a7cf62f16567799e2c
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-21 15:46:40 -0500

    created teacher signUp

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import SignUp from './pages/SignUp';
+import SignUpForm from "./components/SignUpForm/SignUpForm";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
+        <Route exact path='/'>
+          <SignUpForm />
+        </Route>
+  
     </Router>
   );
 }
