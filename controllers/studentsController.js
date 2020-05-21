2e9de297a042730544c3de5462fa4a63f3e4a7d1
Alvin Galit <aroblesgalit@gmail.com>
2020-05-21 14:01:41 -0500

    Created studentsController

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -7,5 +7,30 @@ module.exports = {
             .find(req.query)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        db.Student
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.Student 
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.Student
+            .findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.Student
+            .findById({ _id: req.params })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     }
 };
