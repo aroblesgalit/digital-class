85eb00aa8bf76308a3d7ca00d97c9e5e8d1c257a
Ryan Gautier <ryangautier2@gmail.com>
2020-05-24 18:40:45 -0500

    create leftcolprofile page

--- client/src/components/ProfileLeftCol/index.js
+++ client/src/components/ProfileLeftCol/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './style.css';
 
-function ProfileLeftCol() {
+function ProfileLeftCol(props) {
   return (
-    <div>
-      Hello
+
+    <div className="left-col-container uk-flex uk-flex-column uk-margin uk-margin-left" >
+      <div className="uk-text-large">
+        {props.name}
+      </div>
+      <div className="uk-text-small uk-margin-bottom">
+        {props.school}
+      </div>
+      <img src="https://via.placeholder.com/300x300" alt="Profile Picture" className="profile-picture" />
+      {props.teachers ? (<div className="uk-text-small uk-margin-top">
+        {props.teachers.join(", ")}
+      </div>) : <div></div> }
+      {props.subject ? (<div className="uk-text-small uk-margin-top">
+        {props.subject}
+      </div>) : <div></div> }
+      {props.email ? (<div className="uk-text-small uk-margin-bottom">
+        {props.email}
+      </div>) : <div></div>}
     </div>
   )
 }
--- client/src/pages/TeacherProfile.js
+++ client/src/pages/TeacherProfile.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ProfileLeftCol from '../components/ProfileLeftCol';
 
+const user = {
+  name: "Ryan Gautier",
+  school: "Centennial High School",
+  subject: "AP Physics",
+  email: "ryangautier2@gmail.com"
+}
+
+
 function TeacherProfile() {
  return(
-     <ProfileLeftCol />
+     <ProfileLeftCol {...user}/>
  );
 }
 
