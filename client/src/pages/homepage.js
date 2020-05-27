1d8d5d9fdc430552ca813caae9399bbfdbaab250
Jordan Roenitz <61602629+jroenitz@users.noreply.github.com>
2020-05-27 10:56:47 -0500

    merged

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,59 @@
+
+
+import React from "react";
+import { List, ListItem } from "../components/List";
+
+function Homepage() {
+    return (
+        <div className="uk-padding-large">
+            <div className="uk-grid">
+
+                <div className="uk-width-1-2">
+                    <h1 className="uk-text-large uk-text-center">Digtal Class</h1>
+                    <p className="uk-text-center">lorim nfdsjakm
+                </p> <div className="uk-grid">
+                        <div className="uk-width-1-2">
+                            <div className="uk-card uk-card-default uk-card-body">
+                                <h3 className="uk-card-title">  <div className="member-info">
+                                    <a href="https://github.com/jroenitz" target="_blank" rel="noopener noreferrer">
+                                        <img className="member-thumbnail" src="https://avatars0.githubusercontent.com/u/61602629?s=460&u=1995a6a23ecd93be57fc6a84ac8f61d205401101&v=4" alt="Jordan Roenitz" />
+                                    </a>
+                                    <div>Jordan<br />Roenitz</div>
+                                </div></h3>
+                                <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <button className="uk-button uk-button-default">Teacher</button>
+                            </div>
+                        </div>
+                        <div className="uk-width-1-2">
+                            <div className="uk-card uk-card-default uk-card-body">
+                                <h3 className="uk-card-title">  <div className="member-info">
+                                    <a href="https://github.com/jroenitz" target="_blank" rel="noopener noreferrer">
+                                        <img className="member-thumbnail" src="https://avatars0.githubusercontent.com/u/61602629?s=460&u=1995a6a23ecd93be57fc6a84ac8f61d205401101&v=4" alt="Jordan Roenitz" />
+                                    </a>
+                                    <div>Jordan<br />Roenitz</div>
+                                </div></h3>
+                                <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <button className="uk-button uk-button-default">Student</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="uk-width-1-2"><div className="uk-margin-large uk-background-secondary"></div></div>
+                <div className="uk-width-1-2">
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 1 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 2 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 3 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 4 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                </div>
+                <div className="uk-width-1-2"><h2 className="uk-title uk-text-center">Features</h2><div className="uk-margin-large"><div className="uk-background-secondary" style={{ height: "400px" }}></div></div></div>
+
+
+            </div>
+        </div>
+
+    );
+}
+
+
+export default Homepage
