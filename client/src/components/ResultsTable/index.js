ea58fc3683d097695b0e435d6cadae3f51eae3c6
Ryan Gautier <ryangautier2@gmail.com>
2020-05-28 14:55:27 -0500

    ternary for rendering based on tab

--- client/src/components/ResultsTable/index.js
+++ client/src/components/ResultsTable/index.js
@@ -4,72 +4,78 @@ import './style.css';
 import API from '../../utils/API';
 
 function ResultsTable(props) {
-  const [resultState, setResultState] = useState([{_id: 1}]);
-  const [tabState, setTabState] = useState({tab: "table"});
+  const [resultState, setResultState] = useState([{ _id: 1 }]);
+  const [tabState, setTabState] = useState({ tab: "table" });
 
   useEffect(() => {
     getRestuls();
   }, []);
   const getRestuls = async () => {
     await API.getResultsByQuiz(props.id).then(async (res) => {
-      for (let i = 0; i<res.data.length; i++) {
+      for (let i = 0; i < res.data.length; i++) {
         await API.getStudentById(res.data[i].student).then(student => res.data[i].name = student.data.name);
       }
       setResultState(res.data);
-    
-  })
-};
 
-const handleTabChange = (tab) => {
-  setTabState({...tabState, tab: tab});
-}
+    })
+  };
+
+  const handleTabChange = (tab) => {
+    setTabState({ ...tabState, tab: tab });
+  }
+
+
 
   return (
-    <div className="result-wrapper">
-      <div className="uk-flex uk-flex-between">
-        <div>
-          <Link to="teachers/profile">
-            {"< Back"}
-          </Link>
+    <div>
+      <div className="result-wrapper">
+        <div className="uk-flex uk-flex-between">
+          <div>
+            <Link to="teachers/profile">
+              {"< Back"}
+            </Link>
+          </div>
+          <div>
+            <div className="nav-icon" onClick={() => handleTabChange("table")}>
+              <i className="fas fa-table"></i>
+            </div>
+            <div className="nav-icon" onClick={() => handleTabChange("chart")}>
+              <i className="fas fa-chart-bar"></i>
+            </div>
+          </div>
         </div>
-        <div>
-          <div className="nav-icon" onClick={() => handleTabChange("table")}>
-            <i className="fas fa-table"></i>
-          </div>
-          <div className="nav-icon" onClick={() => handleTabChange("chart")}>
-            <i className="fas fa-chart-bar"></i>
-          </div>
-        </div>        
-      </div>
-      <table className="uk-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Score</th>
-            <th>Feedback</th>
-          </tr>
-        </thead>
-        <tbody>
-          {resultState.map(item => {
-            return (
-              <tr key={item._id}>
-                <td>
-                  <div className="name">
-                    {item.name}
-                  </div>
-                </td>
-                <td>{item.score}</td>
-                <td>
-                  <div className="feedback">
-                    {item.feedback}
-                  </div>
-                </td>
+        {tabState.tab === "table" ? (
+          <table className="uk-table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Score</th>
+                <th>Feedback</th>
               </tr>
-            )
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {resultState.map(item => {
+                return (
+                  <tr key={item._id}>
+                    <td>
+                      <div className="name">
+                        {item.name}
+                      </div>
+                    </td>
+                    <td>{item.score}</td>
+                    <td>
+                      <div className="feedback">
+                        {item.feedback}
+                      </div>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
 
+        ) : <div>Chart</div>}
+      </div>
     </div>
   )
 }
