82eec7821b4265413358798699cfa2110d6e8f06
Jordan Roenitz <61602629+jroenitz@users.noreply.github.com>
2020-05-28 11:56:42 -0500

    added features

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -9,8 +9,8 @@ function Homepage() {
             <div className="uk-grid">
 
                 <div className="uk-width-1-2">
-                    <h1 className="uk-text-large uk-text-center">Digtal Class</h1>
-                    <p className="uk-text-center">lorim nfdsjakm
+                    <h1 className="uk-text-large uk-text-center">Digital Class</h1>
+                    <p className="uk-text-center">Digital Class is a improved way for teacher to create and grade quizzes all at the touch of the keyboard. Students will be able to take the quiz and see their grade where ever they are as long as they have internet access.   
                 </p> <div className="uk-grid">
                         <div className="uk-width-1-2">
                             <div className="uk-card uk-card-default uk-card-body">
@@ -41,10 +41,10 @@ function Homepage() {
 
                 <div className="uk-width-1-2"><div className="uk-margin-large uk-background-secondary"></div></div>
                 <div className="uk-width-1-2">
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 1 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 2 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 3 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Feature 4 </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Create Quizzes </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Grade Quizzes </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Take Quizzes </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
+                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Check Your Grades </h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div></div>
                 </div>
                 <div className="uk-width-1-2"><h2 className="uk-title uk-text-center">Features</h2><div className="uk-margin-large"><div className="uk-background-secondary" style={{ height: "400px" }}></div></div></div>
 
