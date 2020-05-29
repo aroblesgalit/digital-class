bb16ef493dcd045a69cd943e7b085842a59a3888
Jordan Roenitz <61602629+jroenitz@users.noreply.github.com>
2020-05-29 10:45:13 -0500

    side img

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -8,6 +8,8 @@ import stugrade from "../images/stugrades.jpg"
 import quizimage from "../images/107284-ON60QE-338.jpg"
 import grading from "../images/grading.jpg"
 import makequiz from "../images/makequiz.jpg"
+import mainfeature from "../images/mainfeature.jpg"
+import headphones from "../images/headphones.jpg"
 
 
 
@@ -47,14 +49,14 @@ function Homepage() {
                     </div>
                 </div>
 
-                <div className="uk-width-1-2"><div className="uk-margin-large uk-background-secondary"></div></div>
+                <div className="uk-width-1-2"><div className="uk-margin-large "> <img src= {headphones}></img></div></div>
                 <div className="uk-width-1-2">
                     <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"><img src={makequiz}></img></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Create Quizzes </h3>Any teacher will be able to easily create mulitple choice quizzes.  </div></div></div>
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"><img src={grading}></img></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Grade Quizzes </h3>At the click of a button you will see what grade your students got as well as see where they struggles with our quiz analytics.  </div></div></div>
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"> <img src={quizimage}></img></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Take Quizzes </h3>Students can take a quiz from anywhere on any device.   </div></div></div>
-                    <div className="uk-margin-large-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-child-width-expand uk-background-secondary"> <img src={stugrade} uk-img="true" className="quizimage"/></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Check Your Grades </h3>As soon as the student is done taking a quiz they will be able to instantly see their grade. </div></div></div>
+                    <div className="uk-margin-small-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"><img src={grading}></img></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Grade Quizzes </h3>At the click of a button you will see what grade your students got as well as see where they struggles with our quiz analytics.  </div></div></div>
+                    <div className="uk-margin-small-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-background-secondary"> <img src={quizimage}></img></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Take Quizzes </h3>Students can take a quiz from anywhere on any device.   </div></div></div>
+                    <div className="uk-margin-small-top uk-card uk-card-default uk-card-body"><div className="uk-grid"><div className="uk-width-1-6 uk-child-width-expand uk-background-secondary"> <img src={stugrade} uk-img="true" className="quizimage"/></div><div className="uk-width-5-6"> <h3 className="uk-card-title"> Check Your Grades </h3>As soon as the student is done taking a quiz they will be able to instantly see their grade. </div></div></div>
                 </div>
-                <div className="uk-width-1-2"><h2 className="uk-title uk-text-center">Features</h2><div className="uk-margin-large"><div className="uk-background-secondary" style={{ height: "400px" }}></div></div></div>
+                <div className="uk-width-1-2"><h2 className="uk-title uk-text-center">Features</h2><div className="uk-margin-large"><div className="img" style={{ height: "400px" }}><img src= {mainfeature}></img></div></div></div>
 
 
             </div>
