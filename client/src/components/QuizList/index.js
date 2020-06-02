4593005bb2f4e8d4b8e1cdaf3150c13d1da0f928
Ryan Gautier <ryangautier2@gmail.com>
2020-06-02 17:27:37 -0500

    add comments to quiz list

--- client/src/components/QuizList/index.js
+++ client/src/components/QuizList/index.js
@@ -4,24 +4,31 @@ import API from '../../utils/API'
 import './style.css';
 
 function QuizList(props) {
+// initialize user state for storing student id
 const [userState, setUserState] = useState();
+
+// on load, get student id and update user state
 useEffect(() => {
   getUser()
 },[]);
 
-  async function getUser() {
-    if (props.user === "student") {
-      const {data: {id}} = await API.getStudentData();
-      setUserState(id);
-    }
+async function getUser() {
+  if (props.user === "student") {
+    const {data: {id}} = await API.getStudentData();
+    setUserState(id);
   }
+}
   
 
   const myRender = (props) => {
+    // if there is a quiz item in props
     if (props.quizzes !== undefined) {
+      // if props.quizzes is not empty
       if (props.quizzes.length > 0) {
         return (
+          // for each quiz in the array in props
           (props.quizzes.map(item => {
+            // render the quiz card
             return (
               <div className="uk-card uk-card-small uk-card-body uk-card-default quizCard" key={item._id}>
                 <div className="uk-flex uk-flex-column uk-flex-middle card-top">
@@ -33,15 +40,20 @@ useEffect(() => {
                   </div>
                 </div>
                 <div className="card-bottom uk-flex uk-flex-center">
+                  {/* if the user is a teacher */}
                   {props.user === "teacher" ?
+                  // show the view results button
                     <Link to={"/teachers/results/" + item._id} className="result-link uk-button">
                       View Results
                     </Link>
                     : ""}
+                    {/* if the user is a student and the student id is in the results array of the quiz*/}
                   {props.user === "student" && item.students.indexOf(userState) === -1 ?
+                  // show the take quiz button
                     <Link to={"/students/quiz/" + item._id} className="quiz-link uk-button">
                       Take Quiz
                     </Link>
+                    // else the show the disabled quiz-taken button
                     : props.user === "student" && <button class="uk-button uk-button-default" disabled={true}>Quiz Taken</button>}
                 </div>
               </div>
@@ -50,6 +62,7 @@ useEffect(() => {
         )
       }
       else {
+        // if there is no quiz to load
         return (
           <div>No quizzes to display</div>
         )
