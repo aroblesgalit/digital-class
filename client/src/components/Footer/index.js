603005e5512d60aa23bd561d466298c03f58a955
Ryan Gautier <ryangautier2@gmail.com>
2020-06-02 18:12:15 -0500

    switch pictures to stored images instead of urls

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,11 @@
-import React from "react"
-import './style.css'
+import React from "react";
+import './style.css';
+import alvin from '../../images/alvin_thumbnail.jpg';
+import michael from '../../images/michael_thumbnail.png';
+import cynthia from '../../images/cynthia_thumbnail.jpg';
+import jordan from '../../images/jordan_thumbnail.jpg';
+import ryan from '../../images/ryan_thumbnail.png';
+
 
 // this is a sticky footer. The body and html styling needed to make it so are located in /src/index.css
 
@@ -15,35 +21,35 @@ function Footer() {
           {/* info for Alvin */}
           <div className="member-info">
             <a href="https://github.com/aroblesgalit" target="_blank" rel="noopener noreferrer">
-              <img className="member-thumbnail" src="https://avatars2.githubusercontent.com/u/38934010?s=460&u=92262cc7b285cd2d8e8e5216bac5b54c5c989983&v=4" alt="Alvin Galit" />
+              <img className="member-thumbnail" src={alvin} alt="Alvin Galit" />
             </a>
             <div>Alvin<br />Galit</div>
           </div>
           {/* info for Michael */}
           <div className="member-info">
             <a href="https://github.com/mdcassaro" target="_blank" rel="noopener noreferrer">
-              <img className="member-thumbnail" src="https://avatars3.githubusercontent.com/u/60193809?s=400&u=d80ade1797a18813d4d312528780f5b503b65f13&v=4" alt="Michael Cassaro" />
+              <img className="member-thumbnail" src={michael} alt="Michael Cassaro" />
             </a>
             <div>Michael<br />Cassaro</div>
           </div>
            {/* info for Cynthia */}
            <div className="member-info">
             <a href="https://github.com/cynthia-dm1216" target="_blank" rel="noopener noreferrer">
-              <img className="member-thumbnail" src="https://avatars3.githubusercontent.com/u/60992830?s=400&u=0f399e7a7a0e0fff40133f86ec93f6115625a005&v=4" alt="Cynthia Dominguez" />
+              <img className="member-thumbnail" src={cynthia} alt="Cynthia Dominguez" />
             </a>
             <div>Cynthia<br />Dominguez</div>
           </div>
           {/* info for Jordan */}
           <div className="member-info">
             <a href="https://github.com/jroenitz" target="_blank" rel="noopener noreferrer">
-              <img className="member-thumbnail" src="https://avatars0.githubusercontent.com/u/61602629?s=460&u=1995a6a23ecd93be57fc6a84ac8f61d205401101&v=4" alt="Jordan Roenitz" />
+              <img className="member-thumbnail" src={jordan} alt="Jordan Roenitz" />
             </a>
             <div>Jordan<br />Roenitz</div>
           </div>
           {/* info for Ryan */}
           <div className="member-info">
             <a href="https://github.com/ryangautier1" target="_blank" rel="noopener noreferrer">
-              <img className="member-thumbnail" src="https://avatars2.githubusercontent.com/u/35753007?s=460&u=103cbdbd244670bd042aa8f4f72cf893798365f5&v=4" alt="Ryan Gautier" />
+              <img className="member-thumbnail" src={ryan} alt="Ryan Gautier" />
             </a>
             <div>Ryan<br />Gautier</div>
           </div>
